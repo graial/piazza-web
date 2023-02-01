1d0e106aed7331ad3b7cec5512bce89eab201b73
graial <devops.sune@gmail.com>
2023-02-01 12:33:03 +0800

    adjust assets

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,5 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import "@rails/actiontext"
-import "trix"
 import "./controllers"
 
 import Bridge from "./bridge/bridge"
